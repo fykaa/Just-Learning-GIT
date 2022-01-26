715f3f513761f4042263ea70b1f5d4720096d963
Faeka <faeka6@gmail.com>
2022-01-27 03:26:25 +0530

    gsj

--- 1github.c
+++ 1github.c
@@ -201,7 +201,37 @@ hence a modified file needs to be STAGED and COMMITTED again!!!!
                 red M   = shows MODIFIED + unstaged files
                 green D = recently deleted file (deleted using rm)
                 red ??  = for untracked files
-17. To 
+                green A = never committed files
+                                                        an experiment:
+                                                                                                                        
+                                                                modified 1github.c
+                                                                modified 2.c
+                                                                deleted hi.html
+
+                                                                check status -s
+                                                                red M for 1github.c
+                                                                red M for 2.c
+                                                                green D for hi.html
+
+                                                                staging 1github.c 
+                                                                git add 1github.c
+
+                                                                check status -s
+                                                                green M for 1github.c
+                                                                red M for 2.c
+                                                                green D for hi.html
+
+                                                                modifying 1github.c again and save
+
+                                                                check status -s
+                                                                greenM + red M for 1gitub.c
+                                                                red M fir 2.c
+                                                                
+
+
+17. To ignore a file while testing/ammending/pushpull/server par bhejna and lana to save data and make process faster
+        
+        
 
 
 
