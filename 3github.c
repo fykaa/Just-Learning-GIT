b0036bd3b11cf0f1fd7ea20431df216bf657ff8f
Faeka <faeka6@gmail.com>
2022-01-27 07:21:51 +0530

    updating github data

--- 3github.c
+++ 3github.c
@@ -11,6 +11,18 @@ COMMAND to connect local and remote GIT repository
     Typing this in the local git terminal:
     git remote add origin https://github.com/fykaa/Just-Learning-GIT.git
 
+COMMAND to check WHICH REMOTE Repos are connected
+    git remote
+    (this just gives name of the remote repository connected)
+    git remote -v
+    (this also gives the information i.e. url of remote fetch and push)
+
+COMMAND to PUSH 'master' branch in 'origin' repository
+    git push origin master
+    (this is possible only if the origin named repo is public in the server, varna github aapko ye jaankari kyu degi ki ye available hai yanahi hai)
+    (To gain read/write access in a private repo of github you need to change settings)
+    
+    git push -u origin master
 
 
 
