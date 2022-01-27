9536e2c0a95c3d942a90af8b5a4a852e175c7fef
Faeka <faeka6@gmail.com>
2022-01-27 06:42:33 +0530

    hdsi

--- 3github.c
+++ 3github.c
@@ -3,7 +3,13 @@
 Github is a hosting platform BY MICROSOFT
     Alternative GIT hosting platforms are : BITBUCKET GITLAB 
 
+REPOSITORY---A FILE
+    REMOTE repo- a file at the server of the internet
+    LOCAL  repo- a file in our pc
 
+COMMAND to connect local and remote GIT repository
+    Typing this in the local git terminal:
+    git remote add origin https://github.com/fykaa/Just-Learning-GIT.git
 
 
 
